943b9b3cc6a63df6dda3d4dfb9146643a603a75f
GioevArsen <gioev.arsen15@list.ru>
2024-07-11 01:47:42 +0300

    added chat and message models and chat main page

---
+++ DevLink/Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevLink.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DirectMessage()
+        {
+            return View();
+        }
+
+        public IActionResult GroupChat()
+        {
+            return View();
+        }
+    }
+}
