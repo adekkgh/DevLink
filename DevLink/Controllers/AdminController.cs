91dce4be43dfee14bb26cbb372e9b09e05848fa9
George Gussaov <gussaov04@gmail.com>
2024-07-11 01:07:16 +0300

    Добавил админу список зарегестрированных пользователей.

---
+++ DevLink/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using DevLink.Db;
+using DevLink.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevLink.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUsersRepository usersRepository;
+        public AdminController(IUsersRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+        public IActionResult Users()
+        {
+            var users = usersRepository.GetUsers();
+            return View(Mapping.ToUsersViewModel(users));
+        }
+    }
+}
